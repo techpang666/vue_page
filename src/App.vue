3e870363081472939c0f828d37486ac6c5bcf3eb
TECHPANG_dev <TECHPANG_dev@test.com>
2021-01-19 00:40:19 +0800

    test05

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <template>
-  <div>
-    <h1>快乐起飞🚀</h1>
-    <h2>这是基于vue+travis自动部署生成的页面</h2>
-    <h3>编译好慢</h3>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
