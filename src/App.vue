7e9bc59e51d7909927bbea5915f6f8af1e453aa1
TECHPANG_dev <TECHPANG_dev@test.com>
2021-01-19 11:58:16 +0800

    done

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,7 @@
 		<h2>这是基于Travis自动部署的页面</h2>
 		<h2>开启Vue项目的云端化访问</h2>
 		<h2><a href="https://techpang.top/">这里点击开启快乐起飞🚀</a></h2>
-		<h2>🚀🌈⚡🍖🔮☕🤣</h2>
-
-		<details>
-			<summary>这里点一下有小彩蛋🥚</summary>
-			<pre>
-  习大大说 "征途漫漫 惟有奋斗"🚀
-  <br>
-  狭路相逢勇者胜 兄弟萌冲阿🍖
-  <br>
-  这里佛系随缘更新 欢迎下次再来🤣
-  </pre>
-		</details>
+		<h2>🍌🌈⚡🍖🔮☕🤣</h2>
 	</div>
 </template>
 
