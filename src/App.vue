f44106e03f2348580656644c0b3a9a5b1ab71243
TECHPANG_dev <TECHPANG_dev@test.com>
2021-01-19 16:24:12 +0800

    update_app

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,7 @@
 	<div>
 		<h2>这是基于Travis自动部署的页面</h2>
 		<h2>开启Vue项目的云端化访问</h2>
-		<h2><a href="https://techpang.top/">这里点击开启快乐起飞🚀</a></h2>
-		<h2>🍌🌈⚡🍖🔮☕🤣</h2>
+		<h2><a href="https://techpang.top/">点击这里开启快乐起飞🚀</a></h2>
 	</div>
 </template>
 
